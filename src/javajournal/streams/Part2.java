ce60b5ee87cd8f5d30442e8ac57c49c625963bb1
mcaliman <mcaliman@deimos>
2018-05-05 18:38:24 +0200

    refactoring

--- src/javajournal/streams/Part2.java
+++ src/javajournal/streams/Part2.java
@@ -10,7 +10,7 @@ public class Part2 {
 
         IntStream stream = IntStream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19);
         int result = stream.limit(5).filter(u -> (u % 2 == 0)).map(u -> u + 1).reduce((u, v) -> (u + v)).getAsInt();
-        System.out.println("result=" + results);
+        System.out.println("result=" + result);
 
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19);
         result = 0;
